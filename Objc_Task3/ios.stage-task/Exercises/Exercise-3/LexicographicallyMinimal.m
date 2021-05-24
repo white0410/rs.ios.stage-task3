b8caf56c5561c9632d9d168a2483f71f27c35249
Andrei <andrei@Andreis-MacBook-Pro.local>
2021-05-24 23:39:56 +0300

    task3

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,28 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSMutableString *_string1 = [NSMutableString stringWithString:string1];
+    NSMutableString *_string2 = [NSMutableString stringWithString:string2];
+    
+    NSMutableString *result = [NSMutableString stringWithString:@""];
+    
+    while(_string1.length != 0 || _string2.length != 0) {
+        if (_string1.length == 0) {
+            [result appendString:_string2];
+            break;
+        } else if (_string2.length == 0) {
+            [result appendString:_string1];
+            break;
+        }
+        if ([[_string1 substringToIndex:1] UTF8String][0] <= [[_string2 substringToIndex:1] UTF8String][0]) {
+            [result appendString: [_string1 substringToIndex:1]];
+            _string1 = [[_string1 substringFromIndex:1] mutableCopy];
+        } else {
+            [result appendString: [_string2 substringToIndex:1]];
+            _string2 = [[_string2 substringFromIndex:1] mutableCopy];
+        }
+    }
+    return [result copy];
 }
 
 @end
