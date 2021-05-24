dce1fe8c332e84964ae909d8edc0943c046cd619
Andrei <andrei@Andreis-MacBook-Pro.local>
2021-05-24 23:36:09 +0300

    task2

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,57 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
+    NSDictionary *month = [[NSDictionary alloc] init];
+    month = @{@1 : @"January",@2 : @"February",@3 : @"March",@4 : @"April",@5 : @"May",@6 : @"June",@7 : @"July",@8 : @"August",@9 : @"September",@10 : @"October",@11 : @"November",@12 : @"December",};
+    for (int i = 0; i < month.count; i++) {
+        if (monthNumber == i) {
+            return [month objectForKey:@(i)];
+        }
+    }
     return nil;
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+    NSDate * correctDate = [dateFormatter dateFromString:date];
+    dateFormatter.dateFormat = @"dd";
+    return [[dateFormatter stringFromDate:correctDate] integerValue];
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru_BY"];
+    dateFormatter.dateFormat = @"E";
+    return [dateFormatter stringFromDate:date];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    [calendar setFirstWeekday:2];
+    NSDate * actualDate = [NSDate now];
+    
+    NSDateComponents *componentActualWeek = [calendar components:NSCalendarUnitWeekOfYear fromDate:actualDate];
+    NSDateComponents *componentsDateWeek = [calendar components:NSCalendarUnitWeekOfYear fromDate:date];
+    
+    NSDateComponents *componentsActualYear = [calendar components:NSCalendarUnitYear fromDate:actualDate];
+    NSDateComponents *componentsDateYear = [calendar components:NSCalendarUnitYear fromDate:date];
+    
+    if ([componentsDateYear year] == [componentsActualYear year]) {
+        if ([componentActualWeek weekOfYear] == [componentsDateWeek weekOfYear]) {
+            return YES;
+        } else {
+            return NO;
+        }
+    } else {
+        return NO;
+    }
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,26 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    NSInteger player1, player2, player3;
+       if (ratingArray.count < 3) {
+               return 0;
+           }
+           int result = 0;
+           for (int i = 0; i < ratingArray.count - 2; i++) {
+               for (int j = i+1; j < ratingArray.count - 1; j++) {
+                   for (int k = j+1; k < ratingArray.count; k++) {
+                       player1 = ratingArray[i].integerValue;
+                       player2 = ratingArray[j].integerValue;
+                       player3 = ratingArray[k].integerValue;
+
+                       if (player1 < player2 == player2 < player3) {
+                           result++;
+                   
+               }
+                   }
+           }
+           }
+    return result;
 }
 
 @end
